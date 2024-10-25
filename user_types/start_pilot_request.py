21e2dc04575c20dc5da2c233a7ece2aa4042cd8e
umaru <15875339926@139.com>
2024-10-25 16:56:25 +0800

    feat: implement bot domain attribute in StartPilotRequest class
    - Add `enable_bot_domain` attribute to `StartPilotRequest` class with default value `False`
    
    Signed-off-by: umaru <15875339926@139.com>

--- user_types/start_pilot_request.py
+++ user_types/start_pilot_request.py
@@ -12,5 +12,6 @@ class StartPilotRequest(CustomUserType):
     rabbitmq_password: str = ""
     enable_ssl: bool = False
     bot_domain: str = ""
+    enable_bot_domain: bool = False
     enable_node_port: bool = False
     node_port: int = 18080
